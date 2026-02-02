d1f65acf9803a45cf4e2057ee55ec0e46059f129
sadafamiri <sadafamiri090@gmail.com>
2026-02-03 01:49:28 +0430

    third commit

--- countries-explorer/src/App.jsx
+++ countries-explorer/src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const[Countries,setCountries] = useState("")
-  const[loading,setLoading] = useState("false")
-  const[error,setError] =useState("null")
+  const[countries,setCountries] = useState([])
+  const[loading,setLoading] = useState(false)
+  const[error,setError] =useState(null)
   const[search,setSearch] =useState("")
   const[region,setRegion] = useState("all")
+  const trimmedText = search.trim();
+  useEffect(()=>{
+    if(!trimemdText){
+      return;
+    }
 
+    // if()
+    const controller = new AbortController();
+      async function fetchCountries(){
+        
+      }
+  },[trimmedText])
   return (
     <div>
 
