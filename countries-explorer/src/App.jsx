609772475b469960cfb2c529b078996386e622c4
sadafamiri <sadafamiri090@gmail.com>
2026-02-03 00:58:38 +0430

    third commit

---
+++ countries-explorer/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const[Countries,setCountries] = useState("")
+  const[loading,setLoading] = useState("false")
+  const[error,setError] =useState("null")
+  const[search,setSearch] =useState("")
+  const[region,setRegion] = useState("all")
+
+  return (
+    <div>
+
+    </div>
+      
+  )
+}
+
+export default App
---
+++ countries-explorer/src/components/SearchBar.jsx
@@ -0,0 +1,39 @@
+import {Search, X} from "lucide-react";
+export default function SearchBar({search,setSearch,region,setRegion}){
+    const hasText=search.trim().lengnth>0;
+    return(
+        <div className="glass rounded-4 p-3">
+            <div className="input">
+                <span className="input-group-text bg-trasparent text-light border-0">
+                    <Search size={18}/>
+                </span>
+                <input type=" text" className="form-control bg-trasparent text-light border-0" 
+                value={search}
+                onChange={(e)=>{setSearch(e.target.value)}}
+                placeholder="Search Countries..."
+                />
+
+                {hasText ?(
+                    <button 
+                    className="btn btn-sm"
+                    type="button"
+                    onClick={()=>{setSearch("")}}
+                    ><X size={16}/>
+                    </button>
+                ): null}
+
+                <select value={region} onChange={(e) => setRegion(e.target.value)}>
+                    <option value="all">All</option>
+                    <option value="Africa">Africa</option>
+                    <option value="Americas">Americas</option>
+                    <option value="Asia">Asia</option>
+                    <option value="Europe">Europe</option>
+                    <option value="Oceania">Oceania</option>
+
+
+                </select>
+            </div>
+
+        </div>
+    )
+}
